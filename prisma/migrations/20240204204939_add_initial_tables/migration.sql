4ed305b8fb70f2c4d2dfbfa7d539d16b8b1b7402
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-02-04 17:52:01 -0300

    chore: add prisma seed

--- prisma/migrations/20240204204939_add_initial_tables/migration.sql
+++ prisma/migrations/20240204204939_add_initial_tables/migration.sql
@@ -10,7 +10,7 @@ CREATE TABLE "User" (
 CREATE TABLE "Barbershop" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "adddress" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
     "imageUrl" TEXT NOT NULL,
 
     CONSTRAINT "Barbershop_pkey" PRIMARY KEY ("id")
