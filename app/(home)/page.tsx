e1a04f5576d071feabe5e881ad6ee55f681ac9a5
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-02-05 20:59:03 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,17 +2,22 @@ import Image from "next/image";
 import Header from "../_components/header";
 import { format } from 'date-fns'
 import { ptBR } from "date-fns/locale";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <div className="px-5 pt-5" >
+      <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá Treiber</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
         </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
   );
