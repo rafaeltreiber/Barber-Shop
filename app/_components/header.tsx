95ddc9064e296c9b06bf4383f354476200cbcb29
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2024-02-12 10:23:31 -0300

    feat: add auth provder

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import Image from 'next/image'
 
+
 const Header = () => {
+
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
@@ -11,6 +15,7 @@ const Header = () => {
         <Button variant='outline' size='icon' className="h-8 w-8">
           <MenuIcon size={16} />
         </Button>
+
       </CardContent>
     </Card>
   );
